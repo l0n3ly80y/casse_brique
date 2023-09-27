33d7d237a2da75ed87ccca77bbeb894bdacc6e5d
poubelleluciengabaix@gmail.com <poubelleluciengabaix@gmail.com>
2023-09-27 16:26:39 +0200

    structure de base 3

--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 from objects import *
 import pygame
 pygame.init()
-width ,height=800,600 #Taille de la fenetre de jeu
+width ,height=1280,720 #Taille de la fenetre de jeu
 monEcran=pygame.display.set_mode((width ,height ))
-def game():#fonction qui est lancée au debut d'une partie
+def game(monEcran):#fonction qui est lancée au debut d'une partie
     """Cette fonction est toute la partie gameplay du jeu"""
+    running=True#cette variable permet
+    while running:
+        monEcran.fill((100))
+
+
+        pygame.display.update()
+def placer_briques(monEcran):
+    
+    return 0
