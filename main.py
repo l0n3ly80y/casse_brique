ac430bd5c5677fbe969b7d3a017284e539022a62
poubelleluciengabaix@gmail.com <poubelleluciengabaix@gmail.com>
2023-09-27 16:17:41 +0200

    structure de base 2

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from objects import *
 import pygame
 pygame.init()
 width ,height=800,600 #Taille de la fenetre de jeu
+monEcran=pygame.display.set_mode((width ,height ))
 def game():#fonction qui est lancée au debut d'une partie
     """Cette fonction est toute la partie gameplay du jeu"""
