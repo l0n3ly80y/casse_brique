74e8e0b50a31826a6d4381b3426706c996684eee
poubelleluciengabaix@gmail.com <poubelleluciengabaix@gmail.com>
2023-09-27 16:15:21 +0200

    structure de base

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 from objects import *
 import pygame
+pygame.init()
+width ,height=800,600 #Taille de la fenetre de jeu
+def game():#fonction qui est lancée au debut d'une partie
+    """Cette fonction est toute la partie gameplay du jeu"""
