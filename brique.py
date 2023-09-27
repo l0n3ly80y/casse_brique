78412321cd9098783be32aadec5e33cdcc107140
Metacraft <rafy.vec@gmail.com>
2023-09-27 17:28:05 +0200

    Update brique.py
    Update brique.py

--- brique.py
+++ brique.py
@@ -15,17 +15,18 @@ class Brique :
 class Brick():
     #attribut de classe un dictionnaire de couleurs
     color = '70726E'
+    powerups = {1 : 'grosse' , 2 : 'perf' , 3 : 'double'}
 
-    def __init__(self, x, y, color):# le constructeur
-        self.w = 75 # attribut longueur
-        self.h = 20 # attreibut largeur
+    def __init__(self, x:int, y:int, powerup:int):# le constructeur
+        self.largeur = 75 # attribut longueur
+        self.hauteur = 20 # attribut largeur
         self.pos = pygame.Vector2(x, y) # attribut position
-        self.color = color #attribut couleur
+        self.color = Brick.color #attribut couleur
 
     # méthode pour afficher la brique
     def display(self):
         noir=(0,0,0)
         #Rectangle plein coloré
-        pygame.draw.rect(monEcran,self.col,(self.pos.x, self.pos.y, self.w, self.h))
+        pygame.draw.rect(monEcran,self.color,(self.pos.x, self.pos.y, self.largeur, self.hauteur))
         #Rectangle vide dont le contour est d'épaisseur 1
-        pygame.draw.rect(monEcran,noir,(self.pos.x, self.pos.y, self.w, self.h),1)
+        pygame.draw.rect(monEcran,noir,(self.pos.x, self.pos.y, self.largeur, self.hauteur),1)
