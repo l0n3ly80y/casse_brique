d000f8b76e1b7055372756c4a143ded44ee15f74
lucien <poubelleluciengabaix@gmail.com>
2023-09-27 16:36:50 +0200

    structure de base 4, j'ai supprimé objects.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-from objects import *
+from brique import *
+from paddle import *
 import pygame
 pygame.init()
-width ,height=1280,720 #Taille de la fenetre de jeu
-monEcran=pygame.display.set_mode((width ,height ))
-def game(monEcran):#fonction qui est lancée au debut d'une partie
+WIDTH ,HEIGHT=1280,720 #Taille de la fenetre de jeu
+
+monEcran=pygame.display.set_mode((WIDTH ,HEIGHT ))
+score_initial
+def game(monEcran,score_initial):#fonction qui est lancée au debut d'une partie
     """Cette fonction est toute la partie gameplay du jeu"""
     running=True#cette variable permet
     while running:
         monEcran.fill((100))
 
 
+
         pygame.display.update()
 def placer_briques(monEcran):
-    
+
     return 0
